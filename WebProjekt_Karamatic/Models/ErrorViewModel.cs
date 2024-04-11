7b44c6d651ed886d71c036769568c73416b23529
iAteYourCouch <sekaramatic@tsn.at>
2024-04-11 13:07:57 +0200

    Projektdateien hinzufügen.

---
+++ WebProjekt_Karamatic/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace WebProjekt_Karamatic.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebProjekt_Karamatic/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+//halloooooo
+var app = builder.Build();
+
+//DI-Container: ist ein Software teil der bereits im Programm
+//      aktiv ist
+//DI-Container: ihm geben wir die DbManager-Klasse bekannt
+//  immer wenn der Name (hier DbManager) bei einem ctor
+//      angeg. wird, übergibt der DI-Container die von ihm erzeugte
+//      Instanz an den ctor
+if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
