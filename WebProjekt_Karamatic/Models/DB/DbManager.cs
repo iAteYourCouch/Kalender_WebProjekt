bf70774b6c3c3705239da3e505351ea083ea28c2
iAteYourCouch <sekaramatic@tsn.at>
2024-04-18 11:56:35 +0200

    weitere Dateien eingefügt

---
+++ WebProjekt_Karamatic/Models/DB/DbManager.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Web_Grundlagen.Models.DB {
+    public class DbManager : DbContext{
+
+        public DbSet<User> Users { get; set; }
+
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            // für den Pomelo-MySQL-Treiber
+            string connectionString = "Server=localhost;database=kalender_projekt;user=root;password=root";
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+    }
+}
---
+++ WebProjekt_Karamatic/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Web_Grundlagen.Models {
+    //vereinfachte Klasse (keine ctors, ToString, Überprüfung)
+    public class User {
+
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Hobbies { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+}
